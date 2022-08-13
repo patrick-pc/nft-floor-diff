bdb63c91574d0123502f4c0f28dbb9935e4c7ef5
spidΞy <web3slinger@gmail.com>
2022-08-13 16:44:53 -0500

    Add navigation bar

---
+++ components/Navbar.js
@@ -0,0 +1,92 @@
+import { useRouter } from 'next/router'
+import { useRef, useState, useEffect } from 'react'
+import debounce from 'lodash.debounce'
+import axios from 'axios'
+
+const Navbar = () => {
+  const router = useRouter()
+  const inputRef = useRef(null)
+  const [searchList, setSearchList] = useState([])
+
+  useEffect(() => {
+    const keyDownHandler = (e) => {
+      if (e.key === '/') {
+        e.preventDefault()
+        inputRef.current.focus()
+      }
+    }
+    document.addEventListener('keydown', keyDownHandler)
+
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler)
+    }
+  }, [])
+
+  const searchCollection = async (name) => {
+    if (!name) {
+      setSearchList([])
+      return
+    }
+    const res = await axios.get(
+      `https://api.reservoir.tools/search/collections/v1?name=${name}&limit=5`
+    )
+    setSearchList(res.data.collections)
+  }
+
+  const handleClick = (collection) => {
+    router.push(`/${collection.collectionId}`)
+    inputRef.current.value = ''
+    setSearchList([])
+  }
+
+  const handleBlur = () => {
+    inputRef.current.value = ''
+    setSearchList([])
+  }
+
+  return (
+    <div className='flex items-center justify-between border-b-2 border-zinc-800 pb-2 pt-16 mb-16 mx-4'>
+      <div className='flex items-center gap-2'>
+        <img className='h-6 w-6' src='/img/logo.png' />
+
+        <h1 className='text-xl font-medium hidden md:block'>
+          <span className='text-blue-500'>NFT</span>{' '}
+          <span className='text-green-500'>Floor</span>{' '}
+          <span className='text-violet-500'>Diff</span>
+        </h1>
+      </div>
+
+      <div className='relative'>
+        <div className='flex items-center justify-between text-sm text-white bg-zinc-900 rounded-md w-60 p-2'>
+          <input
+            className='placeholder-zinc-700 bg-zinc-900 focus:outline-none px-2'
+            type='text'
+            placeholder='Search...'
+            onChange={debounce((e) => searchCollection(e.target.value), 500)}
+            onBlur={handleBlur}
+            ref={inputRef}
+            maxLength={25}
+          />
+          <kbd className='text-center text-xs text-zinc-700 bg-black border border-zinc-700 border-b-[3px] rounded-md px-1.5'>
+            /
+          </kbd>
+        </div>
+
+        <div className='flex flex-col items-center text-white bg-zinc-900/30 backdrop-blur-3xl absolute top-11 left-0 w-60 z-50'>
+          {searchList.map((collection) => (
+            <div
+              className='flex items-center border border-zinc-800 hover:bg-zinc-800 cursor-pointer w-full gap-2 p-2'
+              key={collection.collectionId}
+              onMouseDown={() => handleClick(collection)}
+            >
+              <img className='h-10 w-10' src={collection.image} />
+              <p className='text-sm'>{collection.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,17 @@
-import { useRouter } from 'next/router'
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import { ethers } from 'ethers'
 import axios from 'axios'
-import debounce from 'lodash.debounce'
 import Head from 'next/head'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
-  const router = useRouter()
-  const inputRef = useRef(null)
   const [collections, setCollections] = useState([])
-  const [searchList, setSearchList] = useState([])
 
   useEffect(() => {
     getTopCollections()
   }, [])
 
   const getTopCollections = async () => {
-    console.log('Fetching...')
     const res = await axios.get(
       'https://api.reservoir.tools/collections/v4?includeTopBid=false&limit=20&sortBy=1DayVolume'
     )
@@ -40,30 +35,7 @@ const Home = () => {
         floorPrice: x2y2.data && ethers.utils.formatEther(x2y2.data),
       }
     }
-    console.log(top20)
     setCollections(top20)
-  }
-
-  const searchCollection = async (name) => {
-    if (!name) {
-      setSearchList([])
-      return
-    }
-    const res = await axios.get(
-      `https://api.reservoir.tools/search/collections/v1?name=${name}&limit=5`
-    )
-    setSearchList(res.data.collections)
-  }
-
-  const handleClick = (collection) => {
-    router.push(`/${collection.collectionId}`)
-    inputRef.current.value = ''
-    setSearchList([])
-  }
-
-  const handleBlur = () => {
-    inputRef.current.value = ''
-    setSearchList([])
   }
 
   return (
@@ -75,37 +47,12 @@ const Home = () => {
       </Head>
 
       <main>
-        <div>
-          <input
-            type='text'
-            placeholder='contract address'
-            onChange={debounce((e) => searchCollection(e.target.value), 500)}
-            onBlur={handleBlur}
-            ref={inputRef}
-            className='input'
-          />
-          <div id='items'>
-            {searchList.map((collection) => (
-              <div
-                key={collection.collectionId}
-                onMouseDown={() => handleClick(collection)}
-              >
-                <img
-                  src={collection.image}
-                  alt={collection.name}
-                  style={{ width: '50px', height: '50px' }}
-                />
-                {collection.name}
-              </div>
-            ))}
-          </div>
-        </div>
-
         <div className='container mx-auto'>
+          <Navbar />
           <div className='flex flex-wrap items-center justify-center gap-8'>
             {collections.map((nft) => (
               <div
-                className='flex flex-col items-center w-80 text-white border border-gray-800 rounded-box cursor-pointer gap-4 p-4'
+                className='flex flex-col items-center w-80 text-white border border-zinc-800 rounded-box cursor-pointer gap-4 p-4'
                 key={nft.id}
                 onClick={() => router.push(`/${nft.id}`)}
               >
@@ -117,14 +64,14 @@ const Home = () => {
                   <div className='flex flex-col text-lg font-thin w-32 gap-2'>
                     <div className='flex items-center justify-between gap-4'>
                       <span>{nft.floors.openSea.floorPrice}</span>
-                      <div className='flex items-center justify-center border border-gray-800 rounded-full p-1.5'>
+                      <div className='flex items-center justify-center border border-zinc-800 rounded-full p-1.5'>
                         <img className='h-3 w-3' src='/img/opensea-mono.png' />
                       </div>
                     </div>
 
                     <div className='flex items-center justify-between gap-3'>
                       <span>{nft.floors.looksRare.floorPrice}</span>
-                      <div className='flex items-center justify-center border border-gray-800 rounded-full p-1.5'>
+                      <div className='flex items-center justify-center border border-zinc-800 rounded-full p-1.5'>
                         <img
                           className='h-3 w-3'
                           src='/img/looksrare-mono.png'
@@ -138,14 +85,14 @@ const Home = () => {
                           ? nft.floors.x2y2.floorPrice
                           : 'Loading...'}
                       </span>
-                      <div className='flex items-center justify-center border border-gray-800 rounded-full p-1.5'>
+                      <div className='flex items-center justify-center border border-zinc-800 rounded-full p-1.5'>
                         <img className='h-3 w-3' src='/img/x2y2-mono.png' />
                       </div>
                     </div>
                   </div>
                 </div>
 
-                <p className='text-xl font-medium'>{nft.name}</p>
+                <p className='text-xl'>{nft.name}</p>
               </div>
             ))}
           </div>
