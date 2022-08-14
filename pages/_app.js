6ff1a700226fd28ffe95211039284b3e96dc4544
spidΞy <web3slinger@gmail.com>
2022-08-13 19:23:52 -0500

    Update title and icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className='min-h-screen w-full'>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>NFT Floor Diff</title>
+        <meta name='description' content='NFT Floor Difference' />
+        <link rel='icon' href='/img/logo.png' />
+      </Head>
+      <div className='min-h-screen w-full'>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </>
   )
 }
 
